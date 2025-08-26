602a95dee76d795932b4b7b16258fb31179dea0e
imkjangid <kapil@codweb.in>
2025-08-26 17:30:31 +0530

    Added constant-var.py in in python-basics

---
+++ python-basics/constant-var.py
@@ -0,0 +1,11 @@
+PI = 3.14159
+MAX_CONNECTIONS = 1000
+BACKGROUND_COLOR = "#FFFFFF"
+
+print(PI)
+print(MAX_CONNECTIONS)
+print(BACKGROUND_COLOR)
+
+# You can still reassign this, but you shouldn't by convention
+PI = 3.14  # This will not cause an error
+print(PI)
