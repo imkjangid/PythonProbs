0f3801263f799028812fbbcbbc4c1a63d5312184
imkjangid <kapil@codweb.in>
2025-08-26 17:30:03 +0530

    Added variables.py in in python-basics

---
+++ python-basics/variables.py
@@ -0,0 +1,24 @@
+# variable declaration
+x = 5
+y = "Hello"
+z = 3.14
+
+print(x)      # Output: 5
+print(y)      # Output: Hello
+print(z)      # Output: 3.14
+
+# x, y, z also known as keywords in the case of variable declaration only
+# but they are not reserved keywords in Python.
+
+programming_name = "python"
+print(programming_name)  # Output: python
+
+age = 30
+name = "XYZ"
+price = 19.99
+is_student = True
+
+print(age)      # Output: 30
+print(name)      # Output: XYZ
+print(price)     # Output: 19.99
+print(is_student)  # Output: True
