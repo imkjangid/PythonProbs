795bf9e5ebdb140db3d89764e839b4846432d8c6
imkjangid <kapil@codweb.in>
2025-08-29 19:06:05 +0530

    added dictionary example

---
+++ data-types/dictionary-1.py
@@ -0,0 +1,4 @@
+mydict = {1: 'One', 2: 'Two', 3: 'Three'}  # 'One' is a value while 1 is the key of an element.
+print(mydict, type(mydict))
+
+print(mydict[3])
---
+++ data-types/dictionary-2.py
@@ -0,0 +1,6 @@
+mydict = {1:'value','key':2} # '1' is the key to access 'value' and 'key' is the key to access '2'
+print(type(mydict))
+
+print("mydict[1] = ", mydict[1]); # try to find the element from key.
+
+print("mydict['key'] = ", mydict['key']);  # try to find the key from the element.
